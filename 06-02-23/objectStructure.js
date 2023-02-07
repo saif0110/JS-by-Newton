58a58a12f84fafdd441a28be323d1e5d6979e090
saif0110 <khanmohdtauheed4@gmail.com>
2023-02-07 17:49:00 +0530

    object structure

---
+++ 06-02-23/objectStructure.js
@@ -0,0 +1,12 @@
+var detail = {
+    fullName: "Tauheed Khan",
+    age: 23,
+    married: false,
+    addrees: undefined
+};
+console.log("Hii, its Tauheed Khan");
+console.log(detail);
+
+// typeof keyword, its not function, it is just a var;
+
+console.log(typeof detail); // object
